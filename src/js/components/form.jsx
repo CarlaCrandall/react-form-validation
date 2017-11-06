f6d37b8863adc5ece35d62e9569e556840890238
Carla Crandall <carlalcrandall@gmail.com>
2017-11-06 11:03:07 -0500

    Initial implementation of simple onBlur validation

--- src/js/components/form.jsx
+++ src/js/components/form.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { validateForm } from '../validation/validator';
+import { validateField, validateForm } from '../validation/validator';
 import { TestFormSchema } from '../validation/schemas';
 
 export default class Form extends React.Component {
@@ -10,14 +10,35 @@ export default class Form extends React.Component {
         super(props);
         this.state = {
             errors: {},
-            form: {}
+            form: {
+                name: '',
+                random: ''
+            }
         };
+    }
+
+    validateField(name, value) {
+        
     }
 
     onSubmit(event) {
         event.preventDefault();
         const { errors, isValid } = validateForm(TestFormSchema, this.state.form);
         this.setState({ errors, isValid });
+    }
+
+    onChange(event) {
+        const { name, value } = event.target;
+        const { form } = this.state;
+        form[name] = value;
+        this.setState({ form });
+    }
+
+    onBlur(event) {
+        const { name, value } = event.target;
+        const { errors } = this.state;
+        errors[name] = validateField(TestFormSchema[name], value);
+        this.setState({ errors });
     }
 
     render() {
@@ -28,13 +49,27 @@ export default class Form extends React.Component {
                 {isValid === false && <h2>INVALID</h2>}
                 <div>
                     <label htmlFor="name">Name</label>
-                    <input id="name" type="text" value={form.name} />
+                    <input 
+                        id="name" 
+                        name="name" 
+                        type="text" 
+                        value={form.name} 
+                        onChange={event => this.onChange(event)}
+                        onBlur={event => this.onBlur(event)} 
+                    />
                     {errors.name}
                 </div>
 
                 <div>
                     <label htmlFor="random">Random</label>
-                    <input id="random" type="text" value={form.random} />
+                    <input 
+                        id="random" 
+                        name="random" 
+                        type="text" 
+                        value={form.random} 
+                        onChange={event => this.onChange(event)}
+                        onBlur={event => this.onBlur(event)} 
+                    />
                     {errors.random}
                 </div>
 
